199b4e7ced48cf7804844105f92897d91e704941
Muhammad Umar Farooq <umar@craveup.com>
2025-03-04 12:30:54 +0500

    fix mobile dropdown height

--- src/components/component/Navbar.tsx
+++ src/components/component/Navbar.tsx
@@ -30,7 +30,7 @@ export function Navbar() {
     <header
       className={cx(
         "fixed inset-x-3 top-4 z-50 mx-auto flex max-w-6xl transform-gpu animate-slide-down-fade justify-center overflow-hidden rounded-xl border border-transparent px-3 py-3 transition-all duration-300 ease-[cubic-bezier(0.16,1,0.3,1.03)] will-change-transform",
-        open === true ? "h-52" : "h-16",
+        open === true ? "h-72" : "h-16",
         scrolled || open === true
           ? "backdrop-blur-nav max-w-3xl border-gray-100 bg-white/80 shadow-xl shadow-black/5 dark:border-white/15 dark:bg-black/70"
           : "bg-white/0 dark:bg-gray-950/0"
@@ -108,7 +108,7 @@ export function Navbar() {
             open ? "opacity-100 h-auto visible" : "opacity-0 h-0 invisible"
           )}
         >
-          <ul className="space-y-4 font-medium" onClick={() => setOpen(false)}>
+          <ul className="space-y-3.5 font-medium" onClick={() => setOpen(false)}>
             <li>
               <Link href={siteConfig.baseLinks.about}>About</Link>
             </li>
